9a6829d8b0a0a37a6366215ab20e1c76fb06eaaf
Mihishi <mihishitharushika@gmail.com>
2025-02-19 19:51:47 +0530

    Week-7

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
+
 import express from 'express';
 
 const app = express();
-//import { productRouter } from './Routes/product.js';
-import {getProducts} from './Appications/product.js';
+import { productRouter } from './Routes/product.js';
 
 app.use(express.json()); // For parsing JSON requests
 
-
-
-app.get('/products', getProducts)
-
-
+app.use('/products', productRouter)
 
 app.listen(8000, () => console.log(`Server running on port ${8000}`));
